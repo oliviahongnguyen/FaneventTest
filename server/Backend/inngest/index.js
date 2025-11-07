fcef6301a8aa3c3c62b12eaae70bcb44d3da6fa6
oliviahongnguyen <149336044+oliviahongnguyen@users.noreply.github.com>
2025-11-07 01:59:05 -0800

    fixes3

--- server/Backend/inngest/index.js
+++ server/Backend/inngest/index.js
@@ -8,11 +8,12 @@ const syncUserCreation = inngest.createFunction(
     {id: 'sync-user-from-clerk'},
     { event: 'clerk/user.created' },
     async ({ event })=>{
-        const {id, first_name, last_name, email_addresses, image_url} = event.data
+        const {id, first_name, last_name, email_addresses, username, image_url} = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
+            username: username,
             image: image_url
         }
         await User.create(userData)
@@ -35,11 +36,12 @@ const syncUserUpdation = inngest.createFunction(
     {id: 'update-user-from-clerk'},
     { event: 'clerk/user.updated' },
     async ({ event })=>{
-        const { id, first_name, last_name, email_addresses, image_url } = event.data
+        const { id, first_name, last_name, email_addresses, username, image_url } = event.data
         const userData = {
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
+            username: username,
             image: image_url
         }
         await User.findByIdAndUpdate(id, userData)
