c0834e740ef6200b87c49b4f786c9b4146903a9d
oliviahongnguyen <149336044+oliviahongnguyen@users.noreply.github.com>
2025-11-07 02:09:30 -0800

    last fix?

--- server/Backend/config/db.js
+++ server/Backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () =>{
     try {
         mongoose.connection.on('connected', ()=> console.log('Database connected'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/fanevent`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/fanevent-db`)
     } catch (error) {
         console.log(error.message);
         
